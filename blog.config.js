9fb0b4f3609cae19515d4345d65344cc9d02822f
LIPJE <126466480+LIPJE@users.noreply.github.com>
2023-08-21 18:09:00 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: '个人记录', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: 'Untlsea@163.com', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_EMAIL: '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: 'https://weibo.com/u/3571715017', // 你的微博个人主页
   CONTACT_TWITTER: 'https://x.com/record1ng_han?s=21', // 你的twitter个人主页
   CONTACT_GITHUB: '', // 你的github个人主页 例如 https://github.com/tangly1024
